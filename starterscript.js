397b8aee0e1dbb56891445ea6266293cf39b73c9
Chris Keesey <chriskeesey@Chriss-MacBook-Pro.local>
2016-12-13 08:06:53 -0800

    didn't change anything

--- starterscript.js
+++ starterscript.js
@@ -22,18 +22,18 @@ function setUpBoard(){
 		}
 		grid.push(innergrid);
 	}
-	
+
 	addTile();
-	
+
 }
 
 function addTile() {
-	//place a 2 on a random spot in the board	
+	//place a 2 on a random spot in the board
 	var x = Math.round(Math.random()*3);
 	var y = Math.round(Math.random()*3);
 	grid[x][y] = "2";
 }
-	
+
 
 function printBoard(){
 	var board = '<br/>' + "*--------------*" + '<br/>';
@@ -46,29 +46,29 @@ function printBoard(){
 		board += "*--------------*";
 		board += '<br/>';
 	}
-	
+
 	//console.log(board);
 	document.getElementById("container").innerHTML = board;
 }
 
 
-//function gets called anytime  a key is pressed 
+//function gets called anytime  a key is pressed
 //e is a special variable
-// that references the event obeject that reads if the user is interacting with 
-//the window 
+//that references the event obeject that reads if the user is interacting with
+//the window
 document.onkeydown = function(e) {
-     
-    //makes it work in internet explorer which uses window.event and not e 
+
+    //makes it work in internet explorer which uses window.event and not e
     e = e || window.event;
 
     //keyCode is actually a character value which we convert to a String
-    //to use triple equals sign 
+    //to use triple equals sign
     if (e.keyCode == UP_ARROW) {
         // up arrow
         moveTilesUp();
-        
+
     }
-    //double equals sign will convert it for us 
+    //double equals sign will convert it for us
     else if (e.keyCode == DOWN_ARROW) {
         // down arrow
         console.log("Pressed down");
@@ -80,14 +80,14 @@ document.onkeydown = function(e) {
     else if (e.keyCode == RIGHT_ARROW) {
        // right arrow
        console.log("Pressed right");
-    } 
+    }
     
     printBoard(); //have to recall print board to get the board to update
 };
 
 function moveTilesUp()
 {
-    
+
     for(var r=0; r < grid.length; r++)
     {
         for(var c=0; c<grid[r].length; c++)
@@ -97,9 +97,9 @@ function moveTilesUp()
                 grid[r-1][c] = grid[r][c];
                 grid[r][c] = "x";
             }
-            
+
         }
-        
-    }   
-    
+
+    }
+
 }
