2b3cd72f5c7b29351b70bae6883ea045985b3d12
Chris Keesey <chriskeesey@Chriss-MacBook-Pro.local>
2017-01-06 10:56:09 -0800

    nothing changed

--- starterscript.js
+++ starterscript.js
@@ -5,6 +5,7 @@ var LEFT_ARROW = '37';
 var RIGHT_ARROW = '39';
 var x = "x";
 var score = 0;
+var emptySpaces = 0;
 
 //As soon as webpage loads run these two functions
 $(document).ready(function(){
@@ -24,17 +25,23 @@ function setUpBoard(){
 		}
 		grid.push(innergrid);
 	}
-
+	emptySpaces = 16;
 	addTile();
 
 }
 
+function endGame(){
+	if (emptySpaces == 0){
+		console.log("EndGame")
+	}
+}
 
 function addTile() {
 	var xx = Math.round(Math.random()*3);
 	var y = Math.round(Math.random()*3);
 	if (grid[xx][y] % 2 != 0){
 	grid[xx][y] = "2";
+	emptySpaces -= 1;
 }
 	else{
 	addTile();
@@ -45,14 +52,14 @@ function addTile() {
 
 
 function printBoard(){
-	var board = '<br/>' + "*--------------*" + '<br/>';
+	var board = '<br/>' + "*-------------------*" + '<br/>';
 	for(var i=0; i<grid.length; i++){
 		board += "|   ";
 		for(var j=0; j<grid[i].length; j++){
 			board += grid[i][j] + "   |   ";
 		}
 		board += '<br/>';
-		board += "*--------------*";
+		board += "*-------------------*";
 		board += '<br/>';
 	}
 
@@ -76,10 +83,10 @@ document.onkeydown = function(e) {
     //to use triple equals sign
     if (e.keyCode == UP_ARROW) {
         // up arrow
-        moveTilesUp();
         combineTilesUp();
         moveTilesUp();
         addTile();
+				endGame();
 
     }
     //double equals sign will convert it for us
@@ -113,8 +120,8 @@ function combineTilesUp(){
 
             if(r !== 0  && grid[r][c] !== x && grid[r-1][c] === grid[r][c])
             {
-							grid[r-1][c] = grid[r][c]*2;
-              grid[r][c] = x;
+							grid[r][c] = grid[r][c]*2;
+              grid[r-1][c] = x;
               score += 1;
               console.log(score);
             }
@@ -130,8 +137,8 @@ function combineTilesDown(){
             if(r !== grid.length-1  && grid[r][c] !== x && grid[r+1][c] === grid[r][c])
             {
 
-                grid[r+1][c] = grid[r][c]*2;
-                grid[r][c] = x;
+                grid[r][c] = grid[r][c]*2;
+                grid[r+1][c] = x;
                 score += 1;
                 console.log(score);
             }
@@ -152,8 +159,8 @@ function combineTilesLeft()
             if(c !== 0  && grid[r][c] !== x && grid[r][c-1] === grid[r][c])
             {
 
-                grid[r][c-1] = grid[r][c]*2;
-                grid[r][c] = x;
+                grid[r][c] = grid[r][c]*2;
+                grid[r][c-1] = x;
                 score += 1;
                 console.log(score);
             }
@@ -173,8 +180,8 @@ function combineTilesRight()
         {
             if(c !== grid[r].length-1  && grid[r][c] !== x && grid[r][c+1] === grid[r][c])
             {
-                grid[r][c+1] = grid[r][c]*2;
-                grid[r][c] = x;
+                grid[r][c] = grid[r][c]*2;
+                grid[r][c+1] = x;
                 score += 1;
                 console.log(score);
 
